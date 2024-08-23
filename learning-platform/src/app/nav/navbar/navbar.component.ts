725786e23f0a61c26131818238a7ef8d451bf955
amslmani <ayma513@gmail.com>
2024-08-23 16:31:07 +0200

    navbar with default css style

--- learning-platform/src/app/nav/navbar/navbar.component.ts
+++ learning-platform/src/app/nav/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+import { Dropdown } from 'bootstrap';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    
+  }
+  toggel(){
+    if(document.getElementById("")?.classList.contains("open")){
+      document.getElementById("")?.classList.remove("open");
+    }
+    else{
+      document.getElementById("")?.classList.add("open");
+    }    
+  }
 }
