555e2891ac4ad44dc48e90c563cb155b49748383
amslmani <ayma513@gmail.com>
2024-08-23 17:15:28 +0200

    new hero for home page

--- learning-platform/src/app/app.module.ts
+++ learning-platform/src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './nav/navbar/navbar.component';
 import { FooterComponent } from './nav/footer/footer.component';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
+import { HeroComponent } from './component/hero/hero.component';
+// import {SingupComp}
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
