977c6468a0d598fa1eb99697d9adebe46f5b07f1
amslmani <ayma513@gmail.com>
2024-09-20 11:34:55 +0200

    start with user login

--- learning-platform/src/app/app-routing.module.ts
+++ learning-platform/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { SignupComponent } from './user/signup/signup.component';
 
 const routes: Routes = [
     {
       path: '',
       component: HomeComponent
+    },{
+      path: 'register',
+      component: SignupComponent
     }
 ];
 
